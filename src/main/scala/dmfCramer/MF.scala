3de4489d5335e8441045e339b06ea218ceb5f9d8
Jianbo Ye <yelpoo@gmail.com>
2014-12-07 00:10:55 -0500

    fix some bugs

--- src/main/scala/dmfCramer/MF.scala
+++ src/main/scala/dmfCramer/MF.scala
@@ -9,11 +9,11 @@ trait MF {
 
   val dimension: Int
   val rows, cols: Int
-  val M: CSCMatrix[Double] // sparse rating matrix 
+  val M: CSCMatrix[Double] // sparse rating matrix
   
   def solve(): Unit
   def predict(i: Int, j: Int): Double
-    
+  
   
   /** testing a new tM */
   def test(tB: List[(Int, Int, Double)]): Unit = {
@@ -27,8 +27,8 @@ trait MF {
 }
 
 
-class discreteMF (val dimension: Int, val size: Int, 
-				  val L: List[(Int, Int, Double)]) extends MF {
+class discreteMF (val dimension: Int, val size: Int,
+  val L: List[(Int, Int, Double)]) extends MF {
   val sigma = 1.0
 
   val M: CSCMatrix[Double] = {
@@ -37,7 +37,7 @@ class discreteMF (val dimension: Int, val size: Int,
     println(r, c, L.length)
     val builder = new CSCMatrix.Builder[Double](rows = r, cols = c)
     L.foreach{case (i,j, s)=>builder.add(i, j, s)}
-    builder.result()        
+    builder.result()
   }
   
   val rows = M.rows
@@ -45,21 +45,21 @@ class discreteMF (val dimension: Int, val size: Int,
   val theta = M.mapActiveValues(x=> 0.0)
   
   // factor
-  val U = DenseMatrix.rand(dimension, rows, new Uniform(-sigma, sigma))  // DenseMatrix.rand(size * dimension, rows, new Uniform(-sigma, sigma)) 
+  val U = DenseMatrix.rand(dimension, rows, new Uniform(-sigma, sigma))  // DenseMatrix.rand(size * dimension, rows, new Uniform(-sigma, sigma))
   val V = DenseMatrix.rand(size * dimension, cols, new Uniform(-sigma, sigma))
   
   // bias factor
-  //val u0 = new DenseMatrix[Double](size, rows) 
-  val v0 = new DenseMatrix[Double](size, cols) 
+  //val u0 = new DenseMatrix[Double](size, rows)
+  val v0 = new DenseMatrix[Double](size, cols)
   
-    
+  
   /** compute the gradient w.r.t. the i-th column of U and j-th column of V */
   private def prob(i: Int, j: Int): DenseVector[Double] = {
     assert(i>=0 && i<rows && j>=0 && j<cols)
 
     val u = U(::, i)
     val v = V(::, j).asDenseMatrix.reshape(dimension, size)
-    val s = exp(sum((v(::,*) :* u).apply(::, *)).toDenseVector += v0(::, j))  
+    val s = exp(sum((v(::,*) :* u).apply(::, *)).toDenseVector += v0(::, j))
     s /= (sum(s))
   }
   
@@ -74,7 +74,7 @@ class discreteMF (val dimension: Int, val size: Int,
   private def gradient(i: Int, j: Int, score: Int) : (DenseVector[Double], DenseVector[Double], DenseVector[Double], Double) = {
     val u = U(::, i) // U(::, i).asDenseMatrix.reshape(dimension, size)
     val v = V(::, j).asDenseMatrix.reshape(dimension, size)
-     
+    
     val s = exp(sum((v(::, *) :* u).apply(::, *)).toDenseVector += v0(::, j))
     val p = s.copy //exp(s);
     p /= (sum(p))
@@ -84,14 +84,14 @@ class discreteMF (val dimension: Int, val size: Int,
     val y2p= Y2(score) :* p
     
     // three Newton Raphson step
-    var ratio = theta(i,j) 
+    var ratio = theta(i,j) // hot-start from cached theta
     var f = sum(exp(y * ratio) :*= yp)
     var df = sum(exp(y * ratio) :*= y2p)
     ratio = ratio - f/df
     for (j<- 0 until 5) {
-    f = sum(exp(y * ratio) :*= yp)
-    df = sum(exp(y * ratio) :*= y2p)
-    ratio = ratio - f/df
+      f = sum(exp(y * ratio) :*= yp)
+      df = sum(exp(y * ratio) :*= y2p)
+      ratio = ratio - f/df
     }
     //assert(!ratio.isNaN())
     theta(i,j) = ratio
@@ -99,7 +99,7 @@ class discreteMF (val dimension: Int, val size: Int,
     // compute gradient
     val ey = exp(y * ratio)
     val q = ey :* p
-       
+    
     val r = sum(q) // risk
     q /= r
     
@@ -115,7 +115,7 @@ class discreteMF (val dimension: Int, val size: Int,
     (sum(dU(*, ::)).toDenseVector, dV.flatten(), dp, -log (r))
   }
   
-   def solve() : Unit = {
+  def solve() : Unit = {
     val dU = new DenseMatrix[Double](dimension, rows)
     val dV = new DenseMatrix[Double](size * dimension, cols)
     //val du0 = new DenseMatrix[Double](size, rows)
@@ -123,39 +123,44 @@ class discreteMF (val dimension: Int, val size: Int,
     
     var totalr: Double = 0
     var regr: Double = 0
-    val delta = 0.001
+    val delta = 0.005 // learning rate
+    val momentum = 0.9
     val activeSize = L.length
-    val regCoeff = 0.1/(sigma * sigma)
-    for (iter <- 0 until 200) {
+    val regCoeff = 1.0/(sigma * sigma)
+    val batchSize: Int = 10000
+
+    println("epoch\treg\trisk\tloss")
+    for (iter <- 0 until 300) {
       
       totalr = 0.0
       regr = regCoeff * (sum(U :* U) + sum(V :* V) ) / 2
-      dU := 0d
-      dV := 0d
-      //du0 :=0d
-      dv0 :=0d
-      M.activeIterator.foreach{
-        case ((i, j), s) => {
-	        val (dui, dvj, dpij, r) = gradient(i, j, s.toInt)
-	        dU(::, i) += dui
-	        dV(::, j) += dvj
-	        //du0(::, i) += dpij
-	        dv0(::, j) += dpij
-	        println(i, j, s)
-	        totalr += r        
-	      }}
-      //println(sqrt(sum(U:*U)), sqrt(sum(dU :* dU)) * delta)
-      U -= (dU *= (delta)) 
-      V -= (dV 
-          
-          *= (delta))
-      U += (U * (regCoeff * delta))
-      V += (V * (regCoeff * delta))
-    
-    
-      //u0 -= ((du0 *= (delta)) )
-      v0 -= ((dv0 *= (delta)) )
-      println(iter, regr/activeSize, totalr/activeSize, (regr + totalr)/activeSize)
+      val batches = util.Random.shuffle(L).grouped(batchSize).toList // shuffling samples and grouped into batches
+      batches.foreach(batch => {
+        dU *= momentum
+        dV *= momentum
+        //du0 :=0d
+        dv0 *=momentum
+        batch.foreach{
+          case (i, j, s) => {
+	    val (dui, dvj, dpij, r) = gradient(i, j, s.toInt)
+	    dU(::, i) += dui
+	    dV(::, j) += dvj
+	    //du0(::, i) += dpij
+	    dv0(::, j) += dpij
+	    //println(i, j, s)
+	    totalr += r
+	  }}
+        //println(sqrt(sum(U:*U)), sqrt(sum(dU :* dU)) * delta)
+        U -= (dU *= (delta))
+        V -= (dV *= (delta))
+        U -= (U * (regCoeff * delta))
+        V -= (V * (regCoeff * delta))
+        
+        //u0 -= ((du0 *= (delta)) )
+        v0 -= (dv0 *= (delta))
+      })
+      println(iter.toString + "\t" + (regr/activeSize).toString + "\t" +
+        (totalr/activeSize).toString + " " ((regr + totalr)/activeSize).toString )
     }
   }
 
@@ -178,12 +183,12 @@ object discreteMFrun {
     val lb: ListBuffer[(Int, Int, Double)] = ListBuffer[(Int, Int, Double)]()
     lines.foreach{s =>
       val elem = s.split("\\s+")
-      if (elem.size>=4) 
+      if (elem.size>=4)
         lb.+=((elem(1).toInt, elem(2).toInt, elem(3).toDouble))
     }
     source.close()
     lb.toList
-    //builder.result()    
+    //builder.result()
   }
   
   def main(argv: Array[String]): Unit = {
